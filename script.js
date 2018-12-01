832b4731314ef2f31fe3239cd6b956075da87089
daniil129dan <daniil129dan@gmail.com>
2018-12-02 00:02:36 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = (prompt('сколько у вас с собой есть денег?'))
+undefined
+let b = (prompt('сколько вы купили яблок?'))
+undefined
+let c = (prompt('сколько вы купили батонов хлеба?'))
+undefined
+let d = (prompt('сколько стоит одно яблоко?'))
+undefined
+let e = (prompt('сколько стоит один батон хлеба?'))
+undefined
+a==b*d+c*e
+false
+a<b*d+c*e
+false
+a>b*d+c*e
+true
